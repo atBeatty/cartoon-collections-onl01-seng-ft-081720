620f8b838c53ab542d1cec7b0b8ce51dec999ba6
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-20 02:07:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(collection)
+  collection.each_with_index do |name, index|
+
+    puts "#{index + 1} #{name}"
+  end
+
   # Your code here
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(collection)
+  new_array = []
+  collection.each do |el|
+    el.capitalize!
+    el << "!"
+    new_array << el
+  end
   # Your code here
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(collection)
+  collection.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
+  end
 end
